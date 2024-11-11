99bf746ea99b3850058d9c3258db867d735683e3
gkoz <genadi.kozarev+github@gmail.com>
2024-11-11 19:15:31 +0000

    adds dynamic line width

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Live:
 Preview:
 
 Keywords:
+- `globalCompositeOperation`
--- script.js
+++ script.js
@@ -6,16 +6,18 @@ const ctx = canvas.getContext('2d');
 ctx.strokeStyle = 'red';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 20;
+ctx.lineWidth = 11;
+// google it and potentially add different effects when the lines are crossing over each other
+// ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
+let dynamicLineWidth = true;
 
 function draw(e) {
     if (!isDrawing) return;
-    console.log(e);
     ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
     // start from
@@ -29,6 +31,10 @@ function draw(e) {
     [lastX, lastY] = [e.offsetX, e.offsetY];
     hue++;
     if (hue > 360) hue = 0;
+    if (ctx.lineWidth >= 60 || ctx.lineWidth <= 10) {
+        dynamicLineWidth = !dynamicLineWidth;
+    }
+    dynamicLineWidth ? ctx.lineWidth++ : ctx.lineWidth--;
 }
 
 canvas.addEventListener('mousemove', draw);
